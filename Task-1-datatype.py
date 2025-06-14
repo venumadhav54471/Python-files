101db8963f019ed43bf5563d7151124f27b91f18
venumadhav54471 <venumadhav54471@gmail.com>
2025-06-14 22:01:03 +0530

    Add files via upload

---
+++ Task-1-datatype.py
@@ -0,0 +1,37 @@
+# 08-5-2025 Task-1
+
+name = "John"
+age = 25
+height = 5.9
+is_student = True
+print(name,type(name))                     #john string
+print(age,type(age))                       #25 integer
+print(height,type(height))                 #5.9 float
+print(is_student,type(is_student))         #True boolean
+
+a = 10
+print(a,type(a))                             #10 int
+a = "ten"
+print(a,type(a))                             #ten string
+
+python=[10, 3.14, "hello", True]               #integer #float #string #boolean
+print(python,type(python))                     #type(python)-->list
+
+Student_name="venu"
+marks=50
+result="pass"
+
+name = "Alice"      
+myage = 20          
+print(name)          
+
+hero = "Mahesh babu"
+healthissue = "heart disease"
+print(hero,"saved many children from",healthissue)
+
+a = 5
+b = 10
+a=b
+print(a)
+b=a-5
+print(b)
